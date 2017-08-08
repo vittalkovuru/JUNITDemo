b79cd58bf87076b36b55ddb1105bdae252ad1a2b
E000542 <E000542@HW1579.cignitiglobal.com>
2017-08-08 16:51:31 +0530

    dfthgh

--- src/test/java/vkovuru/JUNITDemo/SampleTest.java
+++ src/test/java/vkovuru/JUNITDemo/SampleTest.java
@@ -1,24 +1,23 @@
 package vkovuru.JUNITDemo;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
 
 public class SampleTest {
 	
-@Test	
+
 	public void simpleTest() {
 	
 	int a = 2;
 	int b = 3;
-	assertTrue(a+b==5);
+	System.out.println(a+b==5);
 }
 
-@Test	
+	
 public void simpleTest_1() {
 
 int a = 3;
 int b = 3;
-assertTrue(a+b==6);
+
+System.out.println(a+b==6);
 }
 }
